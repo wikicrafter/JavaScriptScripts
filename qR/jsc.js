a110f721ebd41fb0f324a433fcede1ce12757076
Wikicrafter <achba@outlook.com>
2023-02-06 16:33:55 +0400

    up

---
+++ qR/jsc.js
@@ -0,0 +1,5 @@
+console.log(`Single-line comments: These start with // and continue to the end of the line.
+ Everything after // on a line is ignored by the JavaScript interpreter.
+
+Multi-line comments: These start with /* and end with */. 
+Everything between /* and */ is ignored by the JavaScript interpreter.`);
